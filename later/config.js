04d48d2c5cc49a1adf405818629700079c6d01f9
hcds <songshu0616@163.com>
2017-01-05 17:34:06 +0900

    update config

--- later/config.js
+++ later/config.js
@@ -1,3 +1,6 @@
+var later = require('later');
+later.date.localTime();
+
 module.exports = {
 	// 账户
 	"account":
@@ -24,16 +27,38 @@ module.exports = {
 	},
 
 	//タスク時間
-	tasks: [
-	    { time: { h: [7], m: [28], s:[] }, address: { name: "相模大野 in", geo_lat: "35.531664", geo_lon: "139.435499" }, inOut: "1" },
-		{ time: { h: [8], m: [40] }, address: { name: "相模大野 out", geo_lat: "35.531556", geo_lon: "139.435439" }, inOut: "2" },
-		{ time: { h: [9], m: [12] }, address: { name: "町田 in", geo_lat: "35.544766", geo_lon: "139.446115" }, inOut: "1" },
-		{ time: { h: [10], m: [45] }, address: { name: "町田 out", geo_lat: "35.544770", geo_lon: "139.446210" }, inOut: "2" }
-		// { time: { h: [7], m: [11] }, address: { name: "八王子 in", geo_lat: "35.658268", geo_lon: "139.336210" }, inOut: "1" },
-		// { time: { h: [8], m: [23] }, address: { name: "八王子 out", geo_lat: "35.658260", geo_lon: "139.336329" }, inOut: "2" },
-		// { time: { h: [9], m: [10] }, address: { name: "町田 in", geo_lat: "35.544756", geo_lon: "139.446195" }, inOut: "1" },
-		// { time: { h: [10], m: [45] }, address: { name: "町田 out", geo_lat: "35.544750", geo_lon: "139.446290" }, inOut: "2" },
-		// { time: { h: [11], m: [18] }, address: { name: "相模大野 in", geo_lat: "35.531644", geo_lon: "139.435412" }, inOut: "1" },
-		// { time: { h: [12], m: [33] }, address: { name: "相模大野 out", geo_lat: "35.531546", geo_lon: "139.435429" }, inOut: "2" }
-	]
+	// tasks: [
+	//     { time: { h: [15], m: [33], s:[01] }, address: { name: "1 in", geo_lat: "35.531664", geo_lon: "139.435499" }, inOut: "1" },
+	// 	{ time: { h: [15], m: [33], s:[05] }, address: { name: "1 out", geo_lat: "35.531556", geo_lon: "139.435439" }, inOut: "2" },
+	// 	{ time: { h: [15], m: [33], s:[10] }, address: { name: "2 in", geo_lat: "35.544766", geo_lon: "139.446115" }, inOut: "1" },
+	// 	{ time: { h: [15], m: [33], s:[30] }, address: { name: "2 out", geo_lat: "35.544770", geo_lon: "139.446210" }, inOut: "2" }
+	// ]
+
+	createTasks : function(h, m) {
+
+		var t = parseInt(m);
+    	console.log("minute :: " + t);
+
+		var tasks = [];
+		tasks[0] = {time:{h:[h], m:this.random(t + 1, t + 2), s:this.random(0, 60) }, address: { name: "1 in", geo_lat: "35.5316" + this.random(0, 99), geo_lon: "139.435" + this.random(1, 599) }, inOut: "1" },
+		tasks[1] = {time:{h:[h], m:this.random(t + 3, t + 4), s:this.random(0, 60) }, address: { name: "1 out", geo_lat: "35.5315" + this.random(0, 99), geo_lon: "139.435" + this.random(1, 599) }, inOut: "2" },
+		tasks[2] = {time:{h:[h], m:this.random(t + 5, t + 6), s:this.random(0, 60) }, address: { name: "2 out", geo_lat: "35.5447" + this.random(0, 99), geo_lon: "139.4461" + this.random(0, 99), }, inOut: "1" },
+		tasks[3] = {time:{h:[h], m:this.random(t + 7, t + 8), s:this.random(0, 60) }, address: { name: "2 out", geo_lat: "35.5447" + this.random(0, 99), geo_lon: "139.4462" + this.random(0, 99), }, inOut: "2" }
+
+		var now = new Date();
+		var d5 = later.dayOfWeek.val(now);
+		if (d5 != 5) {
+			tasks[2] = {time:{h:[h], m:this.random(t + 9, t + 10), s:this.random(0, 60) }, address: { name: "3 in", geo_lat: "35.5447" + this.random(0, 99), geo_lon: "139.4461" + this.random(0, 99), }, inOut: "1" },
+			tasks[3] = {time:{h:[h], m:this.random(t + 11, t + 12), s:this.random(0, 60) }, address: { name: "3 out", geo_lat: "35.5447" + this.random(0, 99), geo_lon: "139.4462" + this.random(0, 99), }, inOut: "2" }
+		}
+
+		return tasks;
+	},
+
+	random : function(min, max) {
+		var arr = [];
+		arr[0] = Math.floor(Math.random() * (max - min + 1)) + min;
+		return arr;
+	}
 };
+
